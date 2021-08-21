c139ee6bd1154e97a5340fd50a0d76014c2dc24c
harshmittal2210 <harshmittal2210@gmail.com>
2021-08-22 00:31:45 +0530

    Save Feature Added

--- copypaste.cpp
+++ copypaste.cpp
@@ -54,8 +54,52 @@ void CopyPaste::recievedNewTabName(QString newName)
     ui->tabWidget->setTabText(currDoubleClickedTab,newName);
 }
 
+bool CopyPaste::writeAllData(QJsonObject &json){
+
+    json["Author Name"] = "Harsh Mittal";
+    json["Total Tabs"] = allTabPtr.length();
+
+    QJsonArray tabsJSON;
+    for (int i=0;i<allTabPtr.length() ;i++ ) {
+        QJsonObject tabDataJSON;
+
+        // Write data for each tab cell
+        QJsonArray allCelldData;
+        for (int j=0;j<allTabPtr[i]->allCellPtr.length() ;j++ ) {
+            QJsonObject cellData;
+            cellData["Text Data"] = allTabPtr[i]->allCellPtr[j]->getTextData();
+
+            allCelldData.append(cellData);
+        }
+        tabDataJSON["Tab Name"] = ui->tabWidget->tabText(i);
+        tabDataJSON["Total Cells"] = allTabPtr[i]->allCellPtr.length();
+        tabDataJSON["Tab Data"] = allCelldData;
+        tabsJSON.append(tabDataJSON);
+    }
+    json["Data"] = tabsJSON;
+    return true;
+}
 void CopyPaste::on_actionSave_triggered()
 {
+//    QFile file("E:\Git\build-multiCopyPaste-Desktop_Qt_5_15_0_MinGW_64_bit-Debug\debug\test.json");
+//    file.open(QIODevice::ReadOnly | QIODevice::Text);
+//    QJsonParseError JsonParseError;
+//    QJsonDocument JsonDocument = QJsonDocument::fromJson(file.readAll(), &JsonParseError);
+//    file.close();
+
+    QFile file("E:/Git/build-multiCopyPaste-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/debug/test.json");
+    if(!file.open(QFile::WriteOnly)){
+        qWarning("Couldn't open save file.");
+        return;
+    }
+
+    QJsonObject copyPasteObject;
+    writeAllData(copyPasteObject);
+
+    file.write(QJsonDocument(copyPasteObject).toJson());
+    file.close();
+
+    qDebug()<<"Saved File";
 
 }
 
